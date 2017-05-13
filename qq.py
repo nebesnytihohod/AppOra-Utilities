919496c2e19db885c0632bf4226cd8eba25cff9d
Maxim Stasenkov <maksim.stasenkov@gmail.com>
2017-05-14 01:05:41 +0300

    FIX(qq.py; Travis-CI): add static analyzer and code formatter

--- qq.py
+++ qq.py
@@ -5,6 +5,7 @@ import os
 import sys
 import re
 import json
+import logging
 
 valid = []
 not_valid = []
@@ -14,95 +15,171 @@ valid_file = []
 not_valid_file = []
 
 module_name = sys.argv[0]
-
-if sys.argv[1] is None:
-    config_file_name = 'config.json'
-else:
-    config_file_name = sys.argv[1]
-
-with open(config_file_name, 'r') as config_file:
-    config_param = json.loads(config_file.read())
-
-name_res_file = config_param['name_res_file']
-name_file = config_param['name_file']
-#file_size = config_param['file_size']    # 500kB -> 512 000 byte
-dimension = config_param['file_size'][1]
-exclude = config_param['exclude']
-search_substring = config_param['search_substring']
-
-if dimension.lower() == 'kb':
-    file_size = config_param['file_size'][0] * 1024
-elif dimension.lower() == 'mb':
-    file_size = config_param['file_size'][0] * 1024 * 1024
-elif dimension.lower() == 'b':
-    file_size = config_param['file_size'][0]
-else:
-    file_size = 0
+__version__ = "1.1"
 
 
-exclude.append(name_res_file)
-exclude.append(name_file)
-exclude.append(module_name)
-exclude.append(config_file_name)
+def main():
 
-for dir_path, subdir_list, files_list in os.walk(os.getcwd()):
-    for f in files_list:
-        mark = False
-        temp_file_name = f.lower()
-        full_name = os.path.join(os.path.abspath(os.path.normcase(dir_path)), f)
-        for e in exclude:
-            if (temp_file_name.find(e) != -1) or (os.path.getsize(full_name) > file_size):
-                mark = True
-                break
-            
-        if mark:
-            not_valid.append(full_name)
-        else:
-            valid.append(full_name)
+    # создание объекта класса логгер
+    logger = logging.getLogger(__name__)
+    # уровень приоритета начиная с которого начинают выводиться сообщения
+    logger.setLevel(logging.DEBUG)
+    # задаем консольный handler и его уровень
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    # задаем файловый handler и его уровень
+    file_handler = logging.FileHandler('logfile.log', 'w')
+    console_handler.setLevel(logging.WARNING)
+    # формат сообщения при выводе на экран
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # добавление formatter'a к конкретному handler'y
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+    # добавление конкретного handler'a к logger'y
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
 
-with open(name_res_file, 'w') as res_file:
-    for i in valid:
-        res_file.write(i)
-        res_file.write('\n')
+    logger.info("Program started")
+    # Создать объект парсера c возможностью вывода справки –h|--help и номера версии –v|--version
+    cli_parser = argparse.ArgumentParser(
+        description='Great Description To Be Here',
+        add_help=True,
+        version=__version__)
 
-    res_file.write('===================================\n\n')
+    # добавить в парсер параметры – аргументы комм.строки
+    cli_parser.add_argument(
+        '-i',
+        '--interactive',
+        action='store',
+        dest='interactive',
+        help='Interactive mode - specifying the parameters of the utility operator')
+    cli_parser.add_argument(
+        '-c',
+        '--config',
+        action='store',
+        dest='create_config_file',
+        help='Create configuration file')
+    cli_parser.add_argument(
+        '-ncf',
+        '--not_config',
+        action='store',
+        dest='not_use_config_file',
+        help='Not use existing configuration file')
+    cli_parser.add_argument('configuration', help='Specifying')
+    cli_parser.add_argument(
+        'result', help='A file with the result of program using')
 
-    for i in not_valid:
-        res_file.write(i)
-        res_file.write('\n')
+    args = cli_parser.parse_args()
 
-for item in search_substring:
-    regexp_pattern.append(re.compile(item))
-
-for element in valid:
-    mark = False
-    with open(element, 'r') as file_obj:
-        for line in file_obj:
-            for item in regexp_pattern:
-                if re.search(item, line) is None:    # Если тру – то значит не найдено; Если фальш – значит найдено
-                    continue
-                else:
-                    valid_line.append(line)
-                    mark = True
-    
-    if mark:
-        valid_file.append(element)
+    if sys.argv[1] is None:
+        config_file_name = 'config.json'
+        logger.info("Default configuration file")
     else:
-        not_valid_file.append(element)
+        config_file_name = sys.argv[1]
+        logger.info("Existing configuration file")
 
-with open(name_file, 'w') as res_file:
-    for i in valid_line:
-        res_file.write(i)
-        res_file.write('\n')
+    with open(config_file_name, 'r') as config_file:
+        config_param = json.loads(config_file.read())
+        logger.info("Parameters loading...")
 
-    res_file.write('===================================\n\n')
+    name_res_file = config_param['name_res_file']
+    final_res_file = config_param['name_file']
+    dimension = config_param['file_size'][1]
+    exclude_list = config_param['exclude']
+    search_substring = config_param['search_substring']
 
-    for i in valid_file:
-        res_file.write(i)
-        res_file.write('\n')
+    if dimension.lower() == 'kb':
+        file_size = config_param['file_size'][0] * 1024
+    elif dimension.lower() == 'mb':
+        file_size = config_param['file_size'][0] * 1024 * 1024
+    elif dimension.lower() == 'b':
+        file_size = config_param['file_size'][0]
+    else:
+        file_size = 0
 
-    res_file.write('===================================\n\n')
+    exclude_list.append(name_res_file)
+    exclude_list.append(name_file)
+    exclude_list.append(module_name)
+    exclude_list.append(config_file_name)
 
-    for i in not_valid_file:
-        res_file.write(i)
-        res_file.write('\n')
+    logger.info("File search and sort is starting...")
+    fileSort(exclude_list, file_size)
+    logger.info("...file search and sort is finishing")
+
+    outputResult(name_res_file, valid)
+    logger.info("Valid data saved in resulting file")
+
+    outputResult(name_res_file, not_valid)
+    logger.info("Not-Valid data saved in resulting file")
+
+    for item in search_substring:
+        regexp_pattern.append(re.compile(item))
+
+    logger.info("Pattern search is starting...")
+    searchPattern(valid, regexp_pattern)
+    logger.info("...pattern search is finishing")
+
+    outputResult(final_res_file, valid_line)
+    logger.info("Line with searching data saved in resulting file")
+
+    outputResult(final_res_file, valid_file)
+    logger.info("UNC filename with searching data saved in resulting file")
+
+    outputResult(final_res_file, not_valid_file)
+    logger.info(
+        "UNC filename witch not contained data saved in resulting file")
+
+
+def fileSort(exclude, f_size):
+    for dir_path, subdir_list, files_list in os.walk(os.getcwd()):
+        for f in files_list:
+            mark = False
+            temp_file_name = f.lower()
+            full_name = os.path.join(
+                os.path.abspath(os.path.normcase(dir_path)), f)
+            for e in exclude:
+                if (temp_file_name.find(e) != -1) or (
+                        os.path.getsize(full_name) > f_size):
+                    mark = True
+                    break
+
+            if mark:
+                not_valid.append(full_name)
+            else:
+                valid.append(full_name)
+
+
+def outputResult(name_file, result_list):
+    with open(name_file, 'w') as res_file:
+        for i in result_list:
+            res_file.write(i)
+            res_file.write('\n')
+
+        res_file.write('===================================\n\n')
+
+
+def searchPattern(result_list, pattern):
+    for element in result_list:
+        mark = False
+        with open(element, 'r') as file_obj:
+            for line in file_obj:
+                for item in pattern:
+                    if re.search(
+                            item, line
+                    ) is None:  # Если тру – то значит не найдено; Если фальш – значит найдено
+                        continue
+                    else:
+                        valid_line.append(line)
+                        mark = True
+
+        if mark:
+            valid_file.append(element)
+        else:
+            not_valid_file.append(element)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    main()
